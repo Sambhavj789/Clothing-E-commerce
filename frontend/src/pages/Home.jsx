7ac8b8a886d286df411fd48a001e67b3dde77204
Sambhav <sambhavjain8840@gmail.com>
2026-07-10 20:58:38 +0530

    Making header responsive

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,16 +1,12 @@
 import "./Home.css";
+import hero from "../assets/image.png";
+import hero2 from "../assets/image3.png";
 function Home() {
   return (
     <section className="home-section">
       <div className="hero-image">
-        <img
-          src="https://lh3.googleusercontent.com/aida-public/AB6AXuAIKQM6vtrknJutyWCcB00tnimY0T9hL9x8OQzyBa1e4WxldE-lZRVbpGYsuaewCmxzLJr4aAr61R3dt4sxurSDte4QRcRmwzXQ2np-4rfeTvjV7q9tns_e-CVIUYejZ-ZbPWCBZC2GfQnflYcKtatreKEdv0BzFXXckVCewHrtvSi1Q7ZT8ornH_ZXlq9_34lPtZIOGxvM4mtwjM6NIA9gSio1mGZwGMwPCbV_i824ww3W3c1awWk9gMDRIl3t9N92KseBHHDYlvQ"
-          alt=""
-        />
-        <img
-          src="https://lh3.googleusercontent.com/aida-public/AB6AXuB_0Qja3_sVJ3RZvhT1XS2sT4oAKUqrdVdcMxWAZh-1PP8n5NXdV6vyjmHe-BOPBG9ccwNYZiY6AzEkETn4cqBi_TqN3SF8CzTkzdHeUPdr-bUbFEs31gNwhy4F8GT0b1V0RP9T3SyDIzRN9Fvc6XdeWfyQhw49TSrmeCtFGuFEMp9U1drbz1VvQdwYH5IOR76TtvS_7u_PokkbOKINzB8cm4n3tbiyvHS8bxf6ybMUo0_e79Th6UyAtndxgL2jasYRTORJRpCQVAQ"
-          alt=""
-        />
+        <img src={hero} alt="" />
+        <img src={hero2} />
       </div>
     </section>
   );
