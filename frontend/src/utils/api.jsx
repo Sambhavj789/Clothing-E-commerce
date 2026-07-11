68a64fc3554608dd347f6636bdf1d83757a214d9
Sambhav <sambhavjain8840@gmail.com>
2026-07-11 17:48:39 +0530

    Added register and login page

---
+++ frontend/src/utils/api.jsx
@@ -0,0 +1,25 @@
+import axios from "axios";
+
+const BACKEND_BASE_URL = "http://localhost:4000/api/v1";
+const api = axios.create({
+  baseURL: BACKEND_BASE_URL,
+  withCredentials: true,
+  headers: { "Content-Type": "application/json" },
+});
+
+export default api;
+
+// Register API: http://localhost:4000/api/v1/auth/register
+
+/*
+fetch(http://localhost:4000/api/v1/auth/register,{
+method:"post",
+headers: {"Content-Type":"application/json"},
+creadentials: true,
+body: JSON.strigify({})
+})
+*/
+
+/*
+api.post("/auth/register",data,{header:""})
+*/
